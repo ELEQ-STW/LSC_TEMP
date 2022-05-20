15d6ea7e2e7072fdd6445abe7d64180320805541
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2022-05-20 16:26:53 +0200

    Fixing .gitignore error

---
+++ ESP32/wireless/settings.py
@@ -0,0 +1,9 @@
+SSID: str = ''
+PASSWORD: str = ''
+
+# Check if the variables are empty
+if (SSID, PASSWORD) == ('', ''):
+    raise ValueError(
+        "The SSID and PASSWORD for the WiFi-connectors are empty. \
+        \nPlease check the settings.py file in ESP32/wireless/"
+    )
