7b487c7e944ff81359a6b463968a5d2d669eaba8
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2022-06-02 09:56:54 +0200

    Fixed bug

--- ESP32/sensor/settings.py
+++ ESP32/sensor/settings.py
@@ -17,11 +17,11 @@ class SETTINGS:
         # Check if the value set for mode is valid.
         assert 0 <= mode <= 2
 
-        modes: tuple = tuple(
+        modes: tuple = tuple([
             0x00,  # Sleep
             0x01,  # Forced
             0x03,  # Normal
-        )
+        ])
         return modes[mode]
 
     # Filter (IIR) modes [CHAPTER 3.3.3]
@@ -37,13 +37,13 @@ class SETTINGS:
         # Check if the value set for mode is valid
         assert 0 <= mode <= 4
 
-        filter: tuple = tuple(
+        filter: tuple = tuple([
             0x00,  # Filter OFF
             0x01,  # Filter 2
             0x02,  # Filter 4
             0x03,  # Filter 8
             0x04,  # Filter 16
-        )
+        ])
         return filter[mode]
 
     # Oversampling modes [CHAPTER 3.3]
@@ -68,22 +68,22 @@ class SETTINGS:
         # Check if the values set for pressure and temperature are valid.
         assert 0 <= pressure <= 5 and 0 <= temperature <= 5 
 
-        p: tuple = tuple(
+        p: tuple = tuple([
             0x00,  # No oversampling
             0x01,  # Oversampling x1
             0x02,  # Oversampling x2
             0x03,  # Oversampling x4
             0x04,  # Oversampling x8
             0x05,  # Oversampling x16
-        )
-        t: tuple = tuple(
+        ])
+        t: tuple = tuple([
             0x00,  # No oversampling
             0x01,  # Oversampling x1
             0x02,  # Oversampling x2
             0x03,  # Oversampling x4
             0x04,  # Oversampling x8
             0x05,  # Oversampling x16
-        )
+        ])
         return p[pressure], t[temperature]
     
     # Standby settings [CHAPTER 3.6.3]
@@ -102,7 +102,7 @@ class SETTINGS:
         # Check if the value set for time is valid
         assert 0 <= time <= 7
 
-        standby: tuple = tuple(
+        standby: tuple = tuple([
             0x00,  # Standby time: 500 microseconds
             0x01,  # Standby time: 62.5 milliseconds
             0x02,  # Standby time: 125 milliseconds
@@ -111,5 +111,5 @@ class SETTINGS:
             0x05,  # Standby time: 1 second
             0x06,  # Standby time: 2 seconds
             0x07,  # Standby time: 4 seconds
-        )
+        ])
         return standby[time]
