606a7bd0e106bcff22f4f90d643b2b59182f56d6
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2022-06-02 15:09:24 +0200

    Small update

--- ESP32/wireless/wireless.py
+++ ESP32/wireless/wireless.py
@@ -12,7 +12,7 @@ class WLAN:
         self.timeout: int = timeout
         self.wlan: object = network.WLAN(network.STA_IF)
 
-    def active(self, state: bool=None) -> None | str:
+    def active(self, state: bool=None) -> None | bool:
         """
         This method can be used to activate the networking module.
         It also can be used to fetch the current state of the module.
@@ -30,27 +30,35 @@ class WLAN:
             return self.wlan.active()
         self.wlan.active(state)
     
-    def scan(self):
+    def scan(self) -> list:
         return self.wlan.scan()
     
-    def config(self):
+    def config(self) -> bytes:
         return self.wlan.config('mac')
     
-    def isConnected(self):
+    def isConnected(self) -> bool:
         return self.wlan.isconnected()
     
-    def ifconfig(self) -> list:
-        return self.wlan.ifconfig()
+    def ifconfig(self) -> dict:
+        return {
+            param: value for param, value in
+            zip(['IP', 'SUBNET', 'GATEWAY', 'DNS'], self.wlan.ifconfig())
+        }
+    
+    def disconnect(self) -> None:
+        self.wlan.disconnect()
+    
+    def status(self) -> int:
+        return self.wlan.status()
 
     def connect(self) -> None:
         _time = time()
         self.active(True)
         if not self.isConnected():
-            print('Connecting to network...')
             self.wlan.connect(self.SSID, self.PWD)
             while not self.isConnected():
                 if time() - _time >= self.timeout:
-                    print('RESET DEVICE...')
+                    print("ERROR: Connection timeout. Resetting device ...")
                     machine.reset()
 
     def __str__(self) -> str:
