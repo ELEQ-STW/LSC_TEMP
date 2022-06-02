bc0ff07f7dee26c4386a7a5d38ab2c42304af8d1
DutchFakeTuber <sybrandvanloenen@hotmail.com>
2022-06-02 09:57:30 +0200

    Added more functions

--- ESP32/wireless/wireless.py
+++ ESP32/wireless/wireless.py
@@ -10,20 +10,48 @@ class WLAN:
         self.SSID: str = ssid
         self.PWD: str = pwd
         self.timeout: int = timeout
-        self.wlan: object = None
+        self.wlan: object = network.WLAN(network.STA_IF)
 
-    def __str__(self) -> str:
-        return f"Network configuration: {self.wlan.ifconfig()}"
+    def active(self, state: bool=None) -> None | str:
+        """
+        This method can be used to activate the networking module.
+        It also can be used to fetch the current state of the module.
+
+        Usage::
+
+            # Activate module:
+            WLAN().active(True)
+            # Deactivate module:
+            WLAN().active(False)
+            # Fetch state:
+            WLAN().active()
+        """
+        if state is None:
+            return self.wlan.active()
+        self.wlan.active(state)
+    
+    def scan(self):
+        return self.wlan.scan()
+    
+    def config(self):
+        return self.wlan.config('mac')
+    
+    def isConnected(self):
+        return self.wlan.isconnected()
+    
+    def ifconfig(self) -> list:
+        return self.wlan.ifconfig()
 
     def connect(self) -> None:
         _time = time()
-        self.wlan = network.WLAN(network.STA_IF)
-        self.wlan.active(True)
-        if not self.wlan.isconnected():
+        self.active(True)
+        if not self.isConnected():
             print('Connecting to network...')
             self.wlan.connect(self.SSID, self.PWD)
-            while not self.wlan.isconnected():
+            while not self.isConnected():
                 if time() - _time >= self.timeout:
                     print('RESET DEVICE...')
-                    sleep(0.1)  # Give the ESP32 time to print the line before resetting
-                    machine.reset()  # Reset ESP32
+                    machine.reset()
+
+    def __str__(self) -> str:
+        return f"Network configuration: {self.ifconfig()}"
